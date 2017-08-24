6bd3a6f8ec36d63b8e008ecf06f26f53fe57eded
Emil Madsen <sovende@gmail.com>
2017-08-24 14:24:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Machine-setup
+
+Utility script to setup new working environments.
+
+## Repos.txt
+Each repository in this list is pulled, and their setup.sh script is executed.
+
+## Packages.txt
+Each package in this list is apt-get installed
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function setup_repo {
+    repo=$1
+    tmp_folder=$(mktemp -d)
+    echo $tmp_folder
+    git clone $repo $tmp_folder
+    cd $tmp_folder
+    ./setup.sh
+}
+
+cd $DIR
+REPOS="repos.txt"
+while IFS= read -r line; do
+    setup_repo $line
+done < $REPOS
+
+function install_package {
+    package=$1
+    apt-get install -y $package
+}
+
+cd $DIR
+PACKAGES="packages.txt"
+while IFS= read -r line; do
+    install_package $line
+done < $PACKAGES
